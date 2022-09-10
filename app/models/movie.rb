85203d0c60652b2b792a4c501908a8ec4d9b4bc0
Jacob <jacobmjones17@gmail.com>
2022-09-10 17:04:37 -0400

    commit tree 238378f18a4fe4cd613a4b1eac3663d136712cf5

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,9 @@
 class Movie < ActiveRecord::Base
+    def self.find_all_movies_by_year(year)
+    Movie.where(year: year)
+    end
 
+    def self.create_with_title(title)
+        Movie.where(title: title)
+    end
 end
